e7def15aecdc0ba9eed7ea56b10b83dd96c44c1e
ddafich <kinjaz11nts@gmail.com>
2024-04-10 20:15:11 +0700

    update
    20:00 10/4/24

--- hoangvancook/app/src/main/java/com/example/hoangvancook/MainActivity.java
+++ hoangvancook/app/src/main/java/com/example/hoangvancook/MainActivity.java
@@ -13,7 +13,7 @@ import com.example.hoangvancook.Listeners.RandomRecipeResponseListener;
 import com.example.hoangvancook.Models.RandomRecipeApiResponse;
 
 public class MainActivity extends AppCompatActivity {
-
+    ProgressDialog dialog;
     RequestManager manager;
     RandomRecipeAdapter randomRecipeAdapter;
     RecyclerView recyclerView;
@@ -24,7 +24,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        dialog = new ProgressDialog(this);
+        dialog.setTitle("Loading...");
         manager = new RequestManager(this);
         manager.getRandomRecipes(randomRecipeResponseListener);
 
@@ -35,7 +36,7 @@ public class MainActivity extends AppCompatActivity {
     private final RandomRecipeResponseListener randomRecipeResponseListener = new RandomRecipeResponseListener() {
         @Override
         public void didFetch(RandomRecipeApiResponse response, String message) {
-
+            
             recyclerView = findViewById(R.id.recycler_random);
             recyclerView.setHasFixedSize(true);
             recyclerView.setLayoutManager(new GridLayoutManager(MainActivity.this, 1));
