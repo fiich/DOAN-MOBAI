eb9aaa48edd7aa8b21994d40540aae98bfdc4f93
hieubeo0017 <134621984+hieubeo0017@users.noreply.github.com>
2024-04-26 09:35:36 +0700

    bieu beo

--- hoangvancook/app/src/main/java/com/example/hoangvancook/HomeActivity.java
+++ hoangvancook/app/src/main/java/com/example/hoangvancook/HomeActivity.java
@@ -16,6 +16,7 @@ import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
@@ -27,13 +28,10 @@ import com.example.hoangvancook.Adapters.RandomRecipeAdapter;
 import com.example.hoangvancook.Listeners.RandomRecipeResponseListener;
 import com.example.hoangvancook.Listeners.RecipeClickListener;
 import com.example.hoangvancook.Models.RandomRecipeApiResponse;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.chip.Chip;
 import com.google.android.material.chip.ChipGroup;
 
-import java.io.IOException;
-import java.net.InetSocketAddress;
-import java.net.Socket;
-import java.net.SocketAddress;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -48,6 +46,7 @@ public class HomeActivity extends AppCompatActivity {
     List<String> tags = new ArrayList<>();
     NestedScrollView nestedScrollView;
     ProgressDialog dialog;
+    BottomNavigationView bottomNavigationView;
 
 
 
@@ -89,6 +88,7 @@ public class HomeActivity extends AppCompatActivity {
         toggleFilterButton = findViewById(R.id.toggleFilterButton);
         chipFilterScrollView = findViewById(R.id.chipFilterScrollView);
         nestedScrollView = findViewById(R.id.nestedScrollView);
+        bottomNavigationView = findViewById(R.id.bottom_nav);
     }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -97,6 +97,21 @@ public class HomeActivity extends AppCompatActivity {
 
         checkInternetAndLoadData();
         findViews();
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+               if(item.getItemId()==R.id.action_home)
+               {
+
+               }
+                if(item.getItemId()==R.id.action_search)
+                {
+                    Intent intent = new Intent(HomeActivity.this,SearchAcivity.class);
+                    startActivity(intent);
+                }
+                return false;
+            }
+        });
 
         String[] filterOptions = getResources().getStringArray(R.array.tags);
         // Add chips for each item in the string array
