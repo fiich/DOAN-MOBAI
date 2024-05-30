9220d3c995d9362a151505f5ad907540a471cc93
quyethv203 <quyet0202366@huce.edu.vn>
2024-05-30 10:19:45 +0700

    update

--- hoangvancook/app/src/main/java/com/example/hoangvancook/Account_Detail.java
+++ hoangvancook/app/src/main/java/com/example/hoangvancook/Account_Detail.java
@@ -1,25 +1,12 @@
 package com.example.hoangvancook;
 
-import android.app.AlertDialog;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.LinearLayout;
 
 import androidx.activity.EdgeToEdge;
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 
 public class Account_Detail extends AppCompatActivity {
     private final String TAG = "Account_Detail";
